b90fec51db9ce70b50c4d386479acfa85f96c78a
AhadKhan98 <ahadkhan98@hotmail.com>
2019-11-04 19:34:52 -0500

    added buttons and empty methods for event listeners

--- xBrowser/src/xBrowser/xBrowser.java
+++ xBrowser/src/xBrowser/xBrowser.java
@@ -8,3 +8,61 @@ import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.text.html.*;
 
+public class xBrowser extends JFrame
+	
+implements HyperlinkListener {
+	
+	private JButton backBtn, fwdBtn; // Forward and back buttons
+	private JTextField urlTextField; // Text field containing url
+	private JEditorPane displayPane; // Displaying pages
+	private ArrayList pages = new ArrayList(); // All pages visited
+	
+	// Constructor
+	public xBrowser() { 
+		super("xBrowser"); // Sets title
+		setSize(640, 480);
+		addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                exitBrowser();
+            }
+		});
+		
+	// Setting up file menu
+	JMenuBar menuBar = new JMenuBar();
+	JMenu fileMenu = new JMenu("File");
+	fileMenu.setMnemonic(KeyEvent.VK_F);
+	JMenuItem fileExitMenuItem = new JMenuItem("Exit", KeyEvent.VK_X);
+	fileExitMenuItem.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            exitBrowser();
+        }
+    });
+	fileMenu.add(fileExitMenuItem);
+    menuBar.add(fileMenu);
+    setJMenuBar(menuBar);
+	
+    
+    // Setting up forward and back buttons
+    JPanel buttonPanel = new JPanel();
+    // Back Button
+    backBtn = new JButton("< Back");
+    backBtn.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            back();
+        }
+    });
+    backBtn.setEnabled(false);
+    buttonPanel.add(backBtn);
+    // Forward Button
+    fwdBtn = new JButton("Forward >");
+    fwdBtn.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            forward();
+        }
+    });
+    fwdBtn.setEnabled(false);
+    buttonPanel.add(fwdBtn);
+	}
+	
+	
+}
